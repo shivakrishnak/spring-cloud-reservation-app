9c784272b7345ebeddaca66e9265773fbb87e0e4
Shivakrishna Kurremula <Shivakrishna_Kurremula@epam.com>
2020-05-25 21:07:12 +0530

    updated

---
+++ eureka-service/src/test/java/com/ksk/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksk.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
